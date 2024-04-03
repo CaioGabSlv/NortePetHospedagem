0c0e7045a2df92f26c7390ab3d9b39b62ec87eea
Thiago-Muller150 <thiagomuller150@gmail.com>
2024-04-03 11:47:06 -0400

    Co-authored-by: Felipe Santos <FELIPESANTOS205@users.noreply.github.com> Co-authored-by: OjoaoSilva1 <OjoaoSilva1@users.noreply.github.com>

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
+import Login from './src/NavigationScreens/Login'
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+export default function NortePetHospedagem () {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={Login}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
